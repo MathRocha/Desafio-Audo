6fc969f1e337e2de788ec047863bb2cd0222cded
MathRocha <mathrocha335@gmail.com>
2019-08-22 20:48:09 -0300

    home-body-mobile animation

--- src/app/home/home-body/home-body-mobile/home-body-mobile.component.ts
+++ src/app/home/home-body/home-body-mobile/home-body-mobile.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 import { HomeBody } from '../home-body';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home-body-mobile',
   templateUrl: './home-body-mobile.component.html',
-  styleUrls: ['./home-body-mobile.component.scss']
+  styleUrls: ['./home-body-mobile.component.scss'],
+  animations: [
+    trigger('expandableState', [
+      transition(':enter', [
+        style({ height: '0', opacity: 0 }),
+        animate('150ms', style({ height: '*', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ height: '*', opacity: 1 }),
+        animate('150ms', style({ height: '0', opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class HomeBodyMobileComponent extends HomeBody {}
